feb4f7eb89d0bf17112cdb5fb0882f1a88609aa1
Simran <simranmishra1708@gmail.com>
2025-12-24 16:38:13 +0530

    project structure done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Simran Mishra",
+    author_email="simranmishra1992x@gmail.com",
+    packages=find_packages()
+)
